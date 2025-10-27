dac723ff17b35136ab6d6759dbb3379f551c9ac1
Pooja Gajbhiye <poojagajbhiye563@gmail.com>
2025-10-28 02:19:53 +0530

    refactor add function

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -9,32 +9,31 @@ function getCustomDelimiters(delimiterLine) {
 }
 
 function add(numbers) {
-    if (typeof numbers === DataType.STRING) {
-        if (numbers.trim() === '') return 0;
+    if (typeof numbers !== DataType.STRING) return 0;
+    if (numbers.trim() === '') return 0;
 
-        let delimiterPattern = /[\n,]/;
-        let numbersPart = numbers;
+    let delimiterPattern = /[\n,]/;
+    let numbersPart = numbers;
 
-        if (numbers.startsWith("//")) {
-            const parts = numbers.split("\n");
-            const delimiterLine = parts.shift();
-            numbersPart = parts.join("\n");
-            delimiterPattern = getCustomDelimiters(delimiterLine);
-        }
-
-        const numberArray = numbersPart.split(delimiterPattern)
-            .map((num) => num.trim())
-            .filter((num) => num !== "");
-
-        const numericValues = numberArray.map((num) => Number(num)).filter((num) => isFinite(num));
-        const negatives = numericValues.filter((num) => num < 0);
-        if (negatives.length > 0) {
-            throw new Error(`Negatives not allowed: ${negatives.join(", ")}`);
-        }
-
-        const validNumbers = numericValues.filter(num => num <= 1000);
-        return validNumbers.reduce((acc, num) => acc + num, 0);
+    if (numbers.startsWith("//")) {
+        const parts = numbers.split("\n");
+        const delimiterLine = parts.shift();
+        numbersPart = parts.join("\n");
+        delimiterPattern = getCustomDelimiters(delimiterLine);
     }
+
+    const numberArray = numbersPart.split(delimiterPattern)
+        .map((num) => num.trim())
+        .filter((num) => num !== "");
+
+    const numericValues = numberArray.map((num) => Number(num)).filter((num) => isFinite(num));
+    const negatives = numericValues.filter((num) => num < 0);
+    if (negatives.length > 0) {
+        throw new Error(`Negatives not allowed: ${negatives.join(", ")}`);
+    }
+
+    const validNumbers = numericValues.filter(num => num <= 1000);
+    return validNumbers.reduce((acc, num) => acc + num, 0);
 }
 
 module.exports = { add };
