5f2202c37110444682446d77ae27585fef75d38e
Pooja Gajbhiye <poojagajbhiye563@gmail.com>
2025-10-28 01:21:36 +0530

    solve cases for different delimiters

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -2,12 +2,30 @@ const DataType = Object.freeze({
     STRING: 'string'
 });
 
+function getCustomDelimiters(delimiterLine) {
+    const customDelimiter = delimiterLine.slice(2).trim();
+    const escaped = customDelimiter.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+    return new RegExp(`[\\n,${escaped}]`);
+}
+
 function add(numbers) {
     if (typeof numbers === DataType.STRING) {
         if (numbers.trim() === '') return 0;
-        const normalized = numbers.replace(/\n/g, ',');
-        const convertedNumberArray = normalized.split(',').map(num => Number(num.trim()));
-        return convertedNumberArray.reduce((acc, num) => acc + num, 0);
+
+        let delimiterPattern = /[\n,]/;
+        let numbersPart = numbers;
+
+        if (numbers.startsWith("//")) {
+            const parts = numbers.split("\n");
+            const delimiterLine = parts.shift();
+            numbersPart = parts.join("\n");
+            delimiterPattern = getCustomDelimiters(delimiterLine);
+        }
+
+        const convertedNumberArray = numbersPart.split(delimiterPattern)
+            .map((num) => num.trim())
+            .filter((num) => num !== "");
+        return convertedNumberArray.reduce((acc, num) => acc + Number(num), 0);
     }
 }
 
