8cedfc9563979f9ac5206c8ad849a8c504adcaee
Pooja Gajbhiye <poojagajbhiye563@gmail.com>
2025-10-28 01:20:31 +0530

    add test cases for different delimiters

--- tests/string_calculator.test.js
+++ tests/string_calculator.test.js
@@ -1,18 +1,21 @@
 const { add } = require('../src/string_calculator');
 
 describe('add()', () => {
+    // Handle empty string
     test('should return 0 for an empty string', () => {
         const inputString = '';
         const result = add(inputString);
         expect(result).toBe(0);
     });
 
+    // Handle singel number
     test('should return the same number for single number in the string', () => {
         const inputString = '1';
         const result = add(inputString);
         expect(result).toBe(1);
     });
 
+    // Handle multiple numbers
     test('should return the sum for two numbers in the string', () => {
         const inputString = '1,2';
         const result = add(inputString);
@@ -37,9 +40,25 @@ describe('add()', () => {
         expect(result).toBe(248);
     });
 
+    // Handle new lines as delimiters
     test('should handle new lines between numbers (instead of commas)', () => {
         const inputString = '1\n12,2,0\n7,1';
         const result = add(inputString);
         expect(result).toBe(23);
+    });
+
+    // Handle different delimiters
+    // To change the delimiter, the beginning of the string will contain a separate line
+    // that looks like this: "//[delimiter]\n[numbers…]"
+    test('should support semi-colon as delimiters (instead of commas)', () => {
+        const inputString = '//;\n1;2';
+        const result = add(inputString);
+        expect(result).toBe(3);
+    });
+
+    test('should support dollar sign as delimiters (instead of commas)', () => {
+        const inputString = '//$\n1$2$3$4';
+        const result = add(inputString);
+        expect(result).toBe(10);
     });
 });
