c76fa8c7e2ad08ecd90a86a2e96c8b4728742938
Pooja Gajbhiye <poojagajbhiye563@gmail.com>
2025-10-27 23:56:38 +0530

    add test case for single number

--- tests/string_calculator.test.js
+++ tests/string_calculator.test.js
@@ -6,4 +6,10 @@ describe('add()', () => {
         const result = add(inputString);
         expect(result).toBe(0);
     });
+
+    test('should return the same number for single number in the string', () => {
+        const inputString = '1';
+        const result = add(inputString);
+        expect(result).toBe(1);
+    });
 });
